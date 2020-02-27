2a90fb97e73b01f69d0f39130135f5e34e8907f9
Thomas Beaupertuis <thomasbeaupertuis@MacBook-Pro-de-Thomas.local>
2020-02-27 10:28:16 +0100

    add empty components

---
+++ src/components/Listing/EmailListing.js
@@ -0,0 +1,17 @@
+import React, {Component, StyleSheet} from 'react';
+
+export default class EmailListing extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <ul>
+
+            </ul>
+        );
+    }
+}
+
---
+++ src/components/Listing/EmailListingItem.js
@@ -0,0 +1,17 @@
+import React, {Component, StyleSheet} from 'react';
+
+export default class EmailListingItem extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <ul>
+
+            </ul>
+        );
+    }
+}
+
