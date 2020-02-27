5fb81eed5204e4bcbbdaddf9a0737f5019d1bb2e
Mégane Courtas <megane.courtas@gmail.com>
2020-02-27 15:59:53 +0100

    Header component : done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import './assets/css/App.scss';
 import GmailConnect from './components/ConnectBtn';
+import HeaderComponent from "./components/Header/HeaderComponent";
 
 function App() {
   return (
     <div className="App">
-      <GmailConnect/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<GmailConnect/>*/}
+
+        <HeaderComponent/>
     </div>
   );
 }
