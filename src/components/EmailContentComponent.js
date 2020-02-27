6d96c30fdf61958ccd5bfd36049dae99cfb1eb0b
Thomas Beaupertuis <thomasbeaupertuis@MacBook-Pro-de-Thomas.local>
2020-02-27 10:10:33 +0100

    create email content component + form new mail

---
+++ src/components/EmailContentComponent.js
@@ -0,0 +1,15 @@
+import React, {Component, StyleSheet} from 'react';
+
+export default class EmailComponent extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
---
+++ src/components/NewMailForm.js
@@ -0,0 +1,11 @@
+
+import React, {Component} from 'react';
+export default class NewMailForm extends Component{
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
