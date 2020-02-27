cf7cdf809f45d1ea40b18ef9be46befab7943ad0
Mégane Courtas <megane.courtas@gmail.com>
2020-02-27 17:41:31 +0100

    Mailbox navigation : done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './assets/css/App.scss';
 import GmailConnect from './components/ConnectBtn';
 import HeaderComponent from "./components/Header/HeaderComponent";
+import MailboxComponent from "./components/Mailbox/MailboxComponent";
 
 function App() {
-  return (
-    <div className="App">
-      {/*<GmailConnect/>*/}
-
-        <HeaderComponent/>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<GmailConnect/>*/}
+            <HeaderComponent/>
+            <main className={"mainContainer"}><MailboxComponent/>
+            </main>
+        </div>
+    );
 }
 
 export default App;
